3995b44b8c9207742b0b7e01f3c53b1584ffdfe3
Elisabeth Koert <elisabeth.koert@med.uni-goettingen.de>
2025-09-18 17:43:46 +0200

    created the main script that will contain the project plan

---
+++ main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 18 17:28:23 2025
+
+@author: Elisabeth
+"""
+
+import sys
+
+def main():
+
+    
+    print("Main function started")
+    # Define parameters for the model
+    
+    # Run function to create the connectivity matriy
+    #
+    
+    # Build Biran 2 model with the connectivity matrix
+
+
+if __name__ == "__main__":
+    main()
